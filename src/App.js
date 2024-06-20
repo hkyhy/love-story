aaf86dd3c02e0ac875eccf7e021c0341cffe8bcf
jinfanghuang <2283785225@qq.com>
2024-06-20 17:22:14 +0800

    chore: add basename

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
 
-import Layout from "./layout/Layout";
-import {pages} from "./pages";
+import Layout from './layout/Layout';
+import { pages } from './pages';
 
-const {Home, First} = pages;
+const { Home, First } = pages;
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />} >
-        <Route index element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="first" element={<First />} />
-      </Route>
-    </Routes>
-  )
+    <Router basename={process.env.REACT_APP_URI}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="first" element={<First />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
