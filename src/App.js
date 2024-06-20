172705897d9b6b082600aa95864c7d3c66df164b
jinfanghuang <2283785225@qq.com>
2024-06-20 18:10:16 +0800

    chore: add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Layout from "./layout/Layout";
-import {pages} from "./pages";
+import Layout from './layout/Layout';
+import { pages } from './pages';
 
-const {Home, First} = pages;
+const { Home, First } = pages;
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} >
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path="first" element={<First />} />
       </Route>
     </Routes>
-  )
+  );
 };
 
 export default App;
